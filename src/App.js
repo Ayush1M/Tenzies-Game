e11e2fe61add8be16ccb61d9decae3433d878f43
Ayush1M <worklife2442@gmail.com>
2022-11-14 23:57:18 +0530

    Did mapping to array die components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
+import Die from "./Die";
 
 function App() {
-   
+  
+  const [dice, setDice] = useState(allNewDice())
+
+
+  function allNewDice(){
+    const newDice = []
+    for(let i=0; i< 12; i++){
+      newDice.push(
+        Math.ceil(Math.random() * 6)
+      )
+    }
+    return newDice
+  }
+
+  const diceElements = dice.map(die => 
+    <Die
+    value = {die.value}/>)
+
   return (
    <main>
-    
+    <div className="die-container">
+           {diceElements}
+        </div>
    </main>
   );
 }
